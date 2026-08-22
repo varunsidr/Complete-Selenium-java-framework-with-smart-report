ec3021fdd0bb3f886ff0fd7996a728ec4f4f7686
varunsidr <varunsidr@gmail.com>
2026-08-23 00:52:41 +0530

    Update locators in Fire_locators for improved XPath accuracy

--- v1/src/test/java/uistore/Fire_locators.java
+++ v1/src/test/java/uistore/Fire_locators.java
@@ -5,8 +5,8 @@ import org.openqa.selenium.By;
 public class Fire_locators {
 
     // FireHomePage
-    public static final By INSURANCE_PRODUCTS = By.xpath("//a[normalize-space(text())='Insurance Products']");
-    public static final By FIRE_INSURANCE = By.xpath("//span[text()='Fire Insurance']");
+    public static final By INSURANCE_PRODUCTS = By.xpath("//*[contains(normalize-space(.), 'Insurance Products')]");
+    public static final By FIRE_INSURANCE = By.xpath("//*[contains(normalize-space(.), 'Fire Insurance')]");
 
     // FireUserDetails
     public static final By MOBILE_FIELD = By.xpath("(//input[@id='smemobile_hi'])[1]");
